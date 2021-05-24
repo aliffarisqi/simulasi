f7829da886fc1372e9c371f13dcd68cd683af11e
bayualiffarisqi <jakartabanyu@gmail.com>
2021-05-24 20:56:25 +0700

    menambahkan file about

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
             <a class="nav-link" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="about.php">About</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">contact</a>
