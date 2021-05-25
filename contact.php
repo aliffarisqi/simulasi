26a8f975e2613a756a76cc3a5a381416fb9ddfc7
my-name <rilo.pambudi1998@gmail.com>
2021-05-25 12:02:57 +0700

    new menu 'photos'

--- contact.php
+++ contact.php
@@ -30,6 +30,9 @@
           <li class="nav-item">
             <a class="nav-link" href="#">contact</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Photos</a>
+          </li>
         </ul>
       </div>
     </div>
